55c8f7dff72a1f26b44ff6752ccc9a2f7b764b9c
Julio Costa <juliodacostalima@gmail.com>
2025-11-07 20:52:07 -0300

    estrutura if elif else

---
+++ logica_de_programacao_basica/aula16.py
@@ -0,0 +1,25 @@
+# if / elif      / else
+# se / se não se / se não
+
+entrada = input('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar': 
+    print('Você entrou no sistema')
+    print('')
+
+    print('So if she is somewhere near me')
+    print('i hope to god she hears me')
+    print('there is no one else could ever make me feel')
+    print('"im so alive"')
+    print('i hope she never leave me')
+    print('please god you must believe me')
+    print('i have search the universe')
+    print('and find myself within her eyes')
+elif entrada == 'sair': 
+    print('Você saiu no sistema')
+elif entrada == 'nada':
+    print('Nada aconteceu, está satisfeito?')
+else:
+    print('Se decida por favor.')
+
+
